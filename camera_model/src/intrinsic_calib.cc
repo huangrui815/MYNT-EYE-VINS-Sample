86b83d2e548fecb28a3330dcc1ef50d5da6eb2b4
rui.hr <rui.hr@alibaba-inc.com>
2019-01-18 10:12:33 +0800

    add processing timer

--- camera_model/src/intrinsic_calib.cc
+++ camera_model/src/intrinsic_calib.cc
@@ -4,6 +4,7 @@
 #include <iomanip>
 #include <iostream>
 #include <algorithm>
+#include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
